ee819911ee34da1cac0811792c371fe6220e9a02
Thurston Daly <dalythu@gmail.com>
2021-07-27 10:50:54 -0400

    prettified JS

--- js/app.js
+++ js/app.js
@@ -1,39 +1,35 @@
 /* Treehouse FSJS Techdegree
  * Project 4 - OOP Game App
  * app.js */
-
-const phrases = [new Phrase('Thurston'), 
-                 new Phrase("Mailchimp"), 
-                 new Phrase("Treehouse Learning"), 
-                 new Phrase("A real one"), 
-                 new Phrase("Coding is fun")];
-
+const phrases = [new Phrase('Thurston'),
+	new Phrase("Mailchimp"),
+	new Phrase("Treehouse Learning"),
+	new Phrase("A real one"),
+	new Phrase("Coding is fun")
+];
 const missed = 0;
 const activePhrase = 'null';
 const startGameListener = document.getElementById('btn__reset');
 const keys = document.getElementsByClassName('key');
-
 let eventHandler = function(e) {
-    let keyPress = e.key;
-    for (let i = 0; i < keys.length; i++) {
-        if (keys[i].innerHTML === keyPress) {
-                if (keys[i].disabled) {
-                    continue
-                } else {
-                    game.handleInteraction(keys[i]);
-                }
-            }
-        }
-    }
-
+	let keyPress = e.key;
+	for (let i = 0; i < keys.length; i++) {
+		if (keys[i].innerHTML === keyPress) {
+			if (keys[i].disabled) {
+				continue
+			} else {
+				game.handleInteraction(keys[i]);
+			}
+		}
+	}
+}
 startGameListener.addEventListener('click', () => {
-    game = new Game(missed, phrases, activePhrase);
-    game.startGame();
-    document.addEventListener('keyup', eventHandler);
+	game = new Game(missed, phrases, activePhrase);
+	game.startGame();
+	document.addEventListener('keyup', eventHandler);
 })
-
 for (let i = 0; i < keys.length; i++) {
-keys[i].addEventListener('click', (e) => {
-    game.handleInteraction(e.target);
-})
+	keys[i].addEventListener('click', (e) => {
+		game.handleInteraction(e.target);
+	})
 }
